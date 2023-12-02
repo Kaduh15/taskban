528ac7ed0e8d506e382df2f818eb5002b6cd9077
Kadu <kadu.silva2014@gmail.com>
2023-12-01 22:16:25 -0300

    ✨ feat: create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Libre_Franklin as LibreFranklin, K2D } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import SideMenu from '@/components/SideMenu'
+import Header from '@/components/Header'
 
 const libreFranklin = LibreFranklin({
   subsets: ['latin'],
@@ -24,12 +25,13 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body
         className={cn(
-          'min-h-screen min-w-full grid grid-cols-2 grid-rows-2 bg-indigo-200 font-sans antialiased',
+          'min-h-screen min-w-full grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] bg-indigo-200 font-sans antialiased text-gray-500',
           libreFranklin.variable,
           k2d.variable,
         )}
       >
         <SideMenu />
+        <Header />
         {children}
       </body>
     </html>
--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -9,7 +9,7 @@ export default function SideMenu() {
   return (
     <div className="bg-white h-full flex flex-col gap-20 py-14 px-14 row-span-2 max-w-[40%] min-w-fit justify-start items-center">
       <h2 className="font-k2 text-indigo-700 font-bold text-4xl">Taskban</h2>
-      <menu className="grid gap-5 text-gray-500 text-xl">
+      <menu className="grid gap-5 text-gray-500 text-lg">
         <li className='flex gap-5 text-indigo-700'>
           <LayoutDashboard /> Quadro
         </li>

ee0c6d5b15e347c2b23386d5661beb49f4d86c3b
Kadu <kadu.silva2014@gmail.com>
2023-12-02 06:28:39 -0300

    👌style: run lint

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -10,17 +10,17 @@ export default function SideMenu() {
     <div className="bg-white h-full flex flex-col gap-20 py-14 px-14 row-span-2 max-w-[40%] min-w-fit justify-start items-center">
       <h2 className="font-k2 text-indigo-700 font-bold text-4xl">Taskban</h2>
       <menu className="grid gap-5 text-gray-500 text-lg">
-        <li className='flex gap-5 text-indigo-700'>
+        <li className="flex gap-5 text-indigo-700">
           <LayoutDashboard /> Quadro
         </li>
-        <li className='flex gap-5'>
+        <li className="flex gap-5">
           <LayoutList /> Lista
         </li>
-        <li className='flex gap-5'>
+        <li className="flex gap-5">
           <GanttChartSquare />
           Timeline
         </li>
-        <li className='flex gap-5'>
+        <li className="flex gap-5">
           <CalendarDays /> Calendário
         </li>
       </menu>
